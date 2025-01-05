22dbb9522a1edb76e059dc9d52671674359a6cf1
Laurent Meyer <lmeyer@ea4d.com>
2025-01-05 18:16:07 +0100

    Update VTune pbd batch gen.

--- Demos/vtune.bat
+++ Demos/vtune.bat
@@ -1,11 +1,5 @@
-map2pdb.exe -bind:Large.exe Large.map
-map2pdb.exe -bind:Demo01.exe Demo01.map
-map2pdb.exe -bind:Demo02.exe Demo02.map
-map2pdb.exe -bind:Demo03.exe Demo03.map
-map2pdb.exe -bind:Demo04.exe Demo04.map
-map2pdb.exe -bind:Demo05.exe Demo05.map
-map2pdb.exe -bind:Demo06.exe Demo06.map
-map2pdb.exe -bind:Demo07.exe Demo07.map
-map2pdb.exe -bind:Demo08.exe Demo08.map
+map2pdb.exe -bind:Simple.exe Simple.map
+map2pdb.exe -bind:SimpleThreaded.exe SimpleThreaded.map
+map2pdb.exe -bind:FMXReport.exe FMXReport.map
 pause
  
