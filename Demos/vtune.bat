379facb6f29a8c8c1dbf00a242f1e7b6e65b459e
Laurent Meyer <lmeyer@ea4d.com>
2025-01-05 18:13:02 +0100

    Add support to VTune

---
+++ Demos/vtune.bat
@@ -0,0 +1,11 @@
+map2pdb.exe -bind:Large.exe Large.map
+map2pdb.exe -bind:Demo01.exe Demo01.map
+map2pdb.exe -bind:Demo02.exe Demo02.map
+map2pdb.exe -bind:Demo03.exe Demo03.map
+map2pdb.exe -bind:Demo04.exe Demo04.map
+map2pdb.exe -bind:Demo05.exe Demo05.map
+map2pdb.exe -bind:Demo06.exe Demo06.map
+map2pdb.exe -bind:Demo07.exe Demo07.map
+map2pdb.exe -bind:Demo08.exe Demo08.map
+pause
+ 
