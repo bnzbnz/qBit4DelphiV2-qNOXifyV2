e41a312204ef0d120b5a5162fa7296cd80195ff7
Laurent Meyer <qBit@lmeyer.fr>
2025-02-07 12:23:44 +0100

    License & contact

--- API/uJsonX4/uJX4Object.pas
+++ API/uJsonX4/uJX4Object.pas
@@ -1,7 +1,7 @@
 ﻿(*****************************************************************************
 The MIT License (MIT)
 
-Copyright (c) 2020-2025 Laurent Meyer JsonX3@ea4d.com
+Copyright (c) 2020-2025 Laurent Meyer JsonX4@lmeyer.fr
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
